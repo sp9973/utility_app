698487d7c9bdf350dbceb81543817ab8692148ec
sp9973 <singhprashantkumar923@gmail.com>
2026-02-18 17:40:07 +0530

    already changed

--- utility_app/lib/main.dart
+++ utility_app/lib/main.dart
@@ -1,13 +1,39 @@
+import 'dart:async';
+import 'package:firebase_app_check/firebase_app_check.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:utility_app/features/auth/views/splash_screen.dart';
 import 'package:utility_app/firebase_options.dart';
 
-void main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
-  runApp(const MyApp());
+  // Initialize Firebase
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  // >>> ENABLE APP CHECK PROPERLY (IMPORTANT) <<<
+  await FirebaseAppCheck.instance.activate(
+    androidProvider: AndroidProvider.playIntegrity,
+    appleProvider: AppleProvider.deviceCheck,
+    
+  );
+
+  // Catch Flutter errors
+  FlutterError.onError = (FlutterErrorDetails details) {
+    FlutterError.presentError(details);
+  };
+
+  // Run app inside a zone
+  runZonedGuarded(
+    () {
+      runApp(const MyApp());
+    },
+    (error, stack) {
+      print("Zoned Error: $error");
+    },
+  );
 }
 
 class MyApp extends StatelessWidget {
